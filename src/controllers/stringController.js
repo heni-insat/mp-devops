0b130f6f19f29012821139dd11b708b7baab34b5
Heni Yangui <40696130+HunYan-io@users.noreply.github.com>
2024-10-25 14:58:05 +0100

    fix code security

--- src/controllers/stringController.js
+++ src/controllers/stringController.js
@@ -2,6 +2,7 @@ exports.reverse = (req, res) => {
   const { text } = req.query;
   if (!text) return res.status(400).send("No text provided");
   if (typeof text !== "string") return res.status(400).send("Invalid text");
+  res.set("Content-Type", "text/plain");
   res.send(text.split("").reverse().join(""));
 };
 
@@ -9,6 +10,7 @@ exports.uppercase = (req, res) => {
   const { text } = req.query;
   if (!text) return res.status(400).send("No text provided");
   if (typeof text !== "string") return res.status(400).send("Invalid text");
+  res.set("Content-Type", "text/plain");
   res.send(text.toUpperCase());
 };
 
@@ -17,5 +19,6 @@ exports.countVowels = (req, res) => {
   if (!text) return res.status(400).send("No text provided");
   if (typeof text !== "string") return res.status(400).send("Invalid text");
   const count = text.match(/[aeiou]/gi)?.length || 0;
+  res.set("Content-Type", "text/plain");
   res.send(count.toString());
 };
