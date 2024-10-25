b6697ce387f10ea18c4fe1abb528070cda94e33c
Heni Yangui <40696130+HunYan-io@users.noreply.github.com>
2024-10-25 14:53:44 +0100

    fix code quality

--- src/controllers/stringController.js
+++ src/controllers/stringController.js
@@ -1,19 +1,21 @@
-
 exports.reverse = (req, res) => {
-    const { text } = req.query;
-    if (!text) return res.status(400).send('No text provided');
-    res.send(text.split('').reverse().join(''));
+  const { text } = req.query;
+  if (!text) return res.status(400).send("No text provided");
+  if (typeof text !== "string") return res.status(400).send("Invalid text");
+  res.send(text.split("").reverse().join(""));
 };
 
 exports.uppercase = (req, res) => {
-    const { text } = req.query;
-    if (!text) return res.status(400).send('No text provided');
-    res.send(text.toUpperCase());
+  const { text } = req.query;
+  if (!text) return res.status(400).send("No text provided");
+  if (typeof text !== "string") return res.status(400).send("Invalid text");
+  res.send(text.toUpperCase());
 };
 
 exports.countVowels = (req, res) => {
-    const { text } = req.query;
-    if (!text) return res.status(400).send('No text provided');
-    const count = text.match(/[aeiou]/gi)?.length || 0;
-    res.send(count.toString());
+  const { text } = req.query;
+  if (!text) return res.status(400).send("No text provided");
+  if (typeof text !== "string") return res.status(400).send("Invalid text");
+  const count = text.match(/[aeiou]/gi)?.length || 0;
+  res.send(count.toString());
 };
