2d3f680431fd72a150a54e14d992ebe8b8f941cb
Heni Yangui <40696130+HunYan-io@users.noreply.github.com>
2024-12-12 00:35:11 +0100

    fix

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const stringController = require('./controllers/stringController');
+const stringController = require("./controllers/stringController");
 
-app.get('/api/reverse', stringController.reverse);
-app.get('/api/uppercase', stringController.uppercase);
-app.get('/api/count-vowels', stringController.countVowels);
+app.get("/api/reverse", stringController.reverse);
+app.get("/api/uppercase", stringController.uppercase);
+app.get("/api/count-vowels", stringController.countVowels);
+app.get("/api/lowercase", stringController.lowercase);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
